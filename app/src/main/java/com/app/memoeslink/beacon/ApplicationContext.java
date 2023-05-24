ff03959dc4870650cf0dca364ab89118e7422cc1
memoeslink <memoeslink@hotmail.com>
2023-05-24 13:04:31 -0600

    Refactored code.

--- app/src/main/java/com/app/memoeslink/beacon/ApplicationContext.java
+++ app/src/main/java/com/app/memoeslink/beacon/ApplicationContext.java
@@ -1,14 +1,8 @@
 package com.app.memoeslink.beacon;
 
 import android.app.Application;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.os.Build;
-import android.os.LocaleList;
 
 import androidx.appcompat.app.AppCompatDelegate;
-
-import java.util.Locale;
 
 /**
  * Created by Memoeslink on 17/10/2017.
@@ -19,30 +13,5 @@ public class ApplicationContext extends Application {
     public void onCreate() {
         super.onCreate();
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-        setLanguage(); //Change app language according to device
-    }
-
-    @SuppressWarnings("deprecation")
-    private void setLanguage() {
-        String language = Locale.getDefault().getLanguage();
-
-        if (!Locale.getDefault().getLanguage().equals("en") && !Locale.getDefault().getLanguage().equals("es"))
-            language = "en";
-        Locale locale = new Locale(language);
-        Locale.setDefault(locale);
-        Resources res = this.getResources();
-        Configuration config;
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            config = res.getConfiguration();
-            config.setLocale(locale);
-            LocaleList localeList = new LocaleList(locale);
-            LocaleList.setDefault(localeList);
-            config.setLocales(localeList);
-        } else {
-            config = res.getConfiguration();
-            config.setLocale(locale);
-            res.updateConfiguration(config, res.getDisplayMetrics());
-        }
     }
 }
--- app/src/main/java/com/app/memoeslink/beacon/CommonActivity.java
+++ app/src/main/java/com/app/memoeslink/beacon/CommonActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.LocaleList;
 
@@ -34,17 +33,11 @@ public class CommonActivity extends AppCompatActivity {
         Locale.setDefault(locale);
         Resources res = context.getResources();
         Configuration configuration = res.getConfiguration();
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            configuration.setLocale(locale);
-            LocaleList localeList = new LocaleList(locale);
-            LocaleList.setDefault(localeList);
-            configuration.setLocales(localeList);
-            context = context.createConfigurationContext(configuration);
-        } else {
-            configuration.setLocale(locale);
-            context = context.createConfigurationContext(configuration);
-        }
+        configuration.setLocale(locale);
+        LocaleList localeList = new LocaleList(locale);
+        LocaleList.setDefault(localeList);
+        configuration.setLocales(localeList);
+        context = context.createConfigurationContext(configuration);
         return new ContextWrapper(context);
     }
 }
