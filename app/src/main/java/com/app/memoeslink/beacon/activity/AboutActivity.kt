1a997a8925c3bd656f9a63552d646a466750e96e
memoeslink <memoeslink@hotmail.com>
2023-06-25 23:57:44 -0600

    Added privacy policy and terms & conditions.

--- app/src/main/java/com/app/memoeslink/beacon/activity/AboutActivity.kt
+++ app/src/main/java/com/app/memoeslink/beacon/activity/AboutActivity.kt
@@ -1,5 +1,7 @@
 package com.app.memoeslink.beacon.activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.Html
 import android.view.View
@@ -9,6 +11,7 @@ import mehdi.sakout.aboutpage.AboutPage
 import mehdi.sakout.aboutpage.Element
 import java.time.Year
 
+
 class AboutActivity : CommonActivity() {
     private var vAbout: View? = null
 
@@ -17,6 +20,22 @@ class AboutActivity : CommonActivity() {
         super.onCreate(savedInstanceState)
 
         // Define 'About' page
+        val privacyPolicyElement = Element().setTitle(getString(R.string.privacy_policy))
+            .setIconDrawable(R.drawable.ic_privacy)
+            .setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data =
+                    Uri.parse("https://drive.google.com/file/d/1bC2CpTax3_0fHo-PTtyqzdPRjko1oHtH/view?usp=sharing")
+                startActivity(intent)
+            }
+        val termsAndConditionsElement = Element().setTitle(getString(R.string.terms_and_conditions))
+            .setIconDrawable(R.drawable.ic_terms)
+            .setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data =
+                    Uri.parse("https://drive.google.com/file/d/1aNQ4CSGSooNXmgIpLKu1EOnsEF6HL-Gx/view?usp=sharing")
+                startActivity(intent)
+            }
         val backElement = Element().setTitle("⟵")
             .setOnClickListener { finish() }
         val description = Html.fromHtml(
@@ -31,6 +50,8 @@ class AboutActivity : CommonActivity() {
         )
         vAbout = AboutPage(this@AboutActivity)
             .isRTL(false)
+            .addItem(privacyPolicyElement)
+            .addItem(termsAndConditionsElement)
             .addPlayStore("com.app.memoeslink.beacon")
             .addWebsite("https://www.linkedin.com/in/guillermo-almaguer/")
             .addGitHub("memoeslink")
