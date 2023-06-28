047858dbdb9b56da5723eca4f4de2a4589dd0d64
memoeslink <memoeslink@hotmail.com>
2023-06-27 20:12:58 -0600

    Refactored code due to Java 19 upgrade. Removed unused resources.

--- app/src/androidTest/java/com/app/memoeslink/beacon/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/app/memoeslink/beacon/ExampleInstrumentedTest.java
@@ -18,10 +18,9 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
         assertEquals("com.app.memoeslink.beacon", appContext.getPackageName());
     }
 }
